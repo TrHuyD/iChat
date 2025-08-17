13614cc2f83507e72e358dc557faf527c7f9a7da
TrHuyD <20521414@gmail.com>
2025-08-17 21:58:19 +0700

    Remove Unique hash req in Mediafile (postgresql)

---
+++ iChat.Data/Migrations/20250817141111_Remove_Unique_Hash_in_Mediafile.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iChat.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Unique_Hash_in_Mediafile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MediaFiles_Hash",
+                table: "MediaFiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaFiles_Hash",
+                table: "MediaFiles",
+                column: "Hash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MediaFiles_Hash",
+                table: "MediaFiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaFiles_Hash",
+                table: "MediaFiles",
+                column: "Hash",
+                unique: true);
+        }
+    }
+}
